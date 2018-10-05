aae6e6900eaec09aeb8337452f16a7edc6c5c107
Richard Cummings <richardecummings@gmail.com>
2018-10-04 23:13:07 -0400

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Richard Cummings on 2018-10-04.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
